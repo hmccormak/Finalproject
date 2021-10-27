5eecf48437cba6c5101207d0187101aedffcdb0d
hmccormak <82132780+hmccormak@users.noreply.github.com>
2021-10-27 10:05:32 -0400

    Add files via upload

---
+++ scratch.py
@@ -0,0 +1,6 @@
+weight = float(input("Weight: "))
+weightType = input("(K)g or (L)bs ")
+if weightType == 'K':
+    print("Weight = " + str(weight) + "kg")
+else:
+    print("Weight = " + str(weight) + "lbs")
