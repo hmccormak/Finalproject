235a76404f397ebad9de52aff1b51155fb36b5f6
JPHart98 <89784428+JPHart98@users.noreply.github.com>
2021-11-01 16:33:40 -0400

    update to game.py
    I added starter type to both classes

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 class Pokemon():
-    def __init__(self, starter, starter_weakness, starter_health, starter_hit_points):
+    def __init__(self, starter, starter_type, starter_weakness, starter_health, starter_hit_points):
             self.starter = starter
+            self.starter_type = starter_type
             self.starter_weakness = starter_weakness
             self.starter_health = starter_health
             self.starter_hit_points = starter_hit_points
@@ -16,8 +17,9 @@ class Player():
             for line in f:
                 x = line.strip().split(",")
                 starter = x[0]
-                starter_weakness = x[1]
-                starter_health = x[2]
-                starter_hit_points = x[3]
-                self.pokemon[starter] = starter_weakness, starter_health, starter_hit_points
+                starter_type = x[1]
+                starter_weakness = x[2]
+                starter_health = x[3]
+                starter_hit_points = x[4]
+                self.pokemon[starter] = starter_type, starter_weakness, starter_health, starter_hit_points
                
