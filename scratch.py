761e58488ad07b7fbbd709fd5ce6c26b802a4e6a
Voyevoda <hmccormak@protonmail.com>
2021-11-04 14:14:28 -0400

    cleanup

--- scratch.py
+++
@@ -1,7 +0,0 @@
-weight = float(input("Weight: "))
-weightType = input("(K)g or (L)bs ")
-if weightType == 'K':
-    print("Weight = " + str(weight) + "kg")
-else:
-    print("Weight = " + str(weight) + "lbs")
-    #comment here
