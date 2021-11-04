7bfab534cabdb96064c69270024b75d017ff07a1
Voyevoda <hmccormak@protonmail.com>
2021-11-04 14:44:06 -0400

    update

--- CIRA.py
+++ CIRA.py
@@ -1,38 +1,59 @@
-# BATTLE FUNCTION DRAFT
-
 from pygame import mixer
 
-mixer.init()
-mixer.music.load("MEGALOVANIA.mp3")
-mixer.music.play()
+#def main():
+    # controls whole game
 
-atk_list = ["punch", "kick"]
-item_list = ["brain food lunch", "rare candy"]
+#def attack():
+    # use attack from list, determine strength from poke stats
+    # and opponen defence
+    
+#def use_item():
+    # use item from list
 
-choice1_flag = False
-while choice1_flag == False:
-    choice1 = input("Attack or Item?: ")
-    if choice1.lower() == "attack":
-        atk_flag = False
-        while atk_flag == False:
-            a_choice = input(f"select attack: {atk_list}: ")
-            if a_choice in atk_list:
-                # use_atk(a_choice)
-                print("used attack")
-                atk_flag = True
-            else:
-                print("pick an option, weenus")
-        choice1_flag = True
-    elif choice1.lower() == "item":
-        item_flag = False
-        while item_flag == False:
-            i_choice = input(f"select item: {item_list}: ")
-            if i_choice in item_list:
-                # use_item(i_choice)
-                print("used item")
-                item_flag = True
-            else:
-                print("pick an option, pingas")
-        choice1_flag = True
-    else:
-        print("pick an option, dingus")
+#class poke():
+    # poke object, will be used to make a list of them
+
+#class Player(name):
+    # create player object, given preset poke list
+
+#class Opponents():
+    # creates opponents, the final boss is called Cira
+    # not sure if they should have one or many poke
+
+def battle():
+    mixer.init()
+    mixer.music.load("MEGALOVANIA.mp3")
+    mixer.music.play()
+
+    atk_list = ["punch", "kick"]
+    item_list = ["brain food lunch", "rare candy"]
+
+    choice1_flag = False
+    while choice1_flag == False:
+        choice1 = input("<Attack or Item?>: ")
+        if choice1.lower() == "attack":
+            atk_flag = False
+            while atk_flag == False:
+                a_choice = input(f"<Select attack>: {atk_list}: ")
+                if a_choice in atk_list:
+                    # use_atk(a_choice)
+                    print("used attack")
+                    atk_flag = True
+                else:
+                    print("pick an option, weenus")
+            choice1_flag = True
+        elif choice1.lower() == "item":
+            item_flag = False
+            while item_flag == False:
+                i_choice = input(f"<Select item>: {item_list}: ")
+                if i_choice in item_list:
+                    # use_item(i_choice)
+                    print("used item")
+                    item_flag = True
+                else:
+                    print("pick an option, pingas")
+            choice1_flag = True
+        else:
+            print("pick an option, dingus")
+
+battle()
