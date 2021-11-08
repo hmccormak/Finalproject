44d9fd70ac522729f778711791eed1fec76a9ad3
Voyevoda <hmccormak@protonmail.com>
2021-11-08 11:57:54 -0500

    made proper docs fixed up battle()

--- CIRA.py
+++ CIRA.py
@@ -4,35 +4,49 @@ from pygame import mixer
     # controls whole game, ask for players name,
     # goes through battles, (player stats reset and end of match?)
 
-#def attack():
-    # use attack from list, determine strength from poke stats
-    # and opponent stats
+def attack():
+    """Deals damages based off of poke stats,
+    uses strength() to determine advantage between poke types
     
-#def use_item():
-    # use item from list, restore hp or boost stat
-    # stat boost resets at end of round
-
-""" class poke():
-
-    def __init__(self, name, atk, hp, def, speed, movelist):
+    Args:
+        p_poke: player poke
+        o_poke: opponent poke
         
-    poke object, will be used to make a list of them
-    Attributes: name, type, atk, hp, def, speed
+    Returns:
+        (int?/float?) of damage value
+    """
+
+class poke():        
+    """poke object, will be used to make a list of them,
+    attacks and its power will be added into a dictionary, eg (attack: power)
     three types: fire, water, magic
-    water crits fire, fire crits magic, magic crits water """
+    water crits fire, fire crits magic, magic crits water
     
-#class Item():
-
-#class Player():
-    # create player object, given preset poke list
+    Attributes: name, type, atk, hp, def, speed
+    """
     
-# class humanPlayer(Player):
-    # inherits player
+class itemCatalog():
+    """Creates dictionary of items from csv file, items can either heal
+    or boost attack/defense. Item name will be the key, its value and it's
+    a/d/h label will be in a tuple
+    
+    Attributes:
+        items: dictionary of items  
+    """
+    def get_item():
+        """Gets item info from catalog and creates item object
+        
+        Args:
+            name (str): name of item
+        
+        Returns:
+            Item object    
+        """
 
-#class cpuPlayer(Player):
-    # inherits player
-    # creates opponents, the final boss is called Cira
-    # not sure if they should have one or many poke
+class Player():
+    """create player object, given preset poke and item list,
+    CPU players will not have items
+    """
 
 def battle():
     # music for final fight
@@ -48,35 +62,27 @@ def battle():
     
     print("\n\n\n\n\n\n\n\n\n\n--++==## THE FIGHT BEGINS ##==++--\n")
     
-    print("opponent_name sends out op_poke_name!\n")
-    print("player_name sends out poke_name!\n")
+    print(f"opponent_name sends out op_poke_name!\n")
+    print(f"player_name sends out poke_name!\n")
 
-    choice1_flag = False
-    while choice1_flag == False:
-        choice1 = input("<Attack or Item?>: ")
-        if choice1.lower() == "attack":
-            atk_flag = False
-            while atk_flag == False:
-                a_choice = input(f"\n<Select attack>: {atk_list}: ")
-                if a_choice in atk_list:
-                    # use_atk(a_choice)
-                    print(f"\n~~> used {a_choice}!")
-                    atk_flag = True
-                else:
-                    print("\n~~> Pick an option, weenus.")
-            choice1_flag = True
-        elif choice1.lower() == "item":
-            item_flag = False
-            while item_flag == False:
-                i_choice = input(f"\n<Select item>: {item_list}: ")
-                if i_choice in item_list:
-                    # use_item(i_choice)
-                    print(f"\n~~> used {i_choice}!")
-                    item_flag = True
-                else:
-                    print("\n~~> Pick an option, pingas.")
-            choice1_flag = True
+    choice_flag = False
+    while choice_flag == False:
+        choice = input("<Attack or Item?>: ")
+        if choice.lower() == "attack":
+            a_choice = input(f"<Select item>: {atk_list}: ")
+            choice_flag = bool(check_select(a_choice, atk_list, choice_flag))
+        elif choice.lower() == "item":
+            i_choice = input(f"<Select item>: {item_list}: ")
+            choice_flag = bool(check_select(i_choice, item_list, choice_flag))
         else:
-            print("\n~~> Pick an option, dingus.\n")
+            print("~~> Pick an option, dingus.")
 
+def check_select(choice, list, choice_flag):
+    if str(choice) in list:
+        print(f"~~> used {choice}!")
+        choice_flag = True
+        return (choice_flag)
+    else:
+        print("~~> Pick an option, dingus.")
+        
 battle()
